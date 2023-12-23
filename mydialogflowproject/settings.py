cb199cd70ae9b19909942e97600f0fd859a8d109
Junaid Girkar <juanidgirkar@gmail.com>
2023-12-23 09:11:43 +0000

    Push from server

--- mydialogflowproject/settings.py
+++ mydialogflowproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@p158mjwhhu@_y^%q&x_3qq@#v)ns$^u86t9$18858*z!t^=oo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 APPS_DIR=True
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
